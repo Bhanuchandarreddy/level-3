060c5c8f444fb046f8277cbd721bafd1dc4b529a
Bhanuchandarreddy <51435957+Bhanuchandarreddy@users.noreply.github.com>
2019-11-27 16:19:48 +0530

    Add files via upload

---
+++ funny.py
@@ -0,0 +1,8 @@
+marks=144
+def predict(x):
+    return (x*2)+1
+
+def propername(first,last):
+    a=first.strip().capitalize()
+    b=last.strip().capitalize()
+    return a+" "+b
---
+++ hello.py
@@ -0,0 +1,4 @@
+import sys
+print('value 1:',sys.argv[1])
+print('value 2:',sys.argv[])
+print('no of inputs:',len(sys.argv))
